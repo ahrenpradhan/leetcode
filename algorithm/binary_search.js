acbb1938c3c7a4d5a95f1c411b212923855a2f02
ahrenpradhan <ahrenpradhan@gmail.com>
2022-04-02 18:34:47 +0530

    fix binary

--- algorithm/binary_search.js
+++ algorithm/binary_search.js
@@ -1,5 +1,8 @@
 const testCase = [
   {
+    input: [[5], 5],
+    output: 0
+  }, {
     input: [[-1, 0, 3, 5, 9, 12], 9],
     output: 4
   }, {
@@ -7,10 +10,32 @@ const testCase = [
     output: -1
   }
 ]
-
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 const binary_search = (nums, target) => {
-  console.log('this is binary_search2');
-  return true;
+  length = nums.length;
+  s_index = 0;
+  e_index = length - 1;
+  while (s_index <= e_index) {
+    pivot_index = Math.round((e_index + s_index) / 2);
+    if (nums[pivot_index] === target) {
+      return pivot_index;
+    }
+    // else if (nums[s_index] === target) {
+    //   return s_index;
+    // } else if (nums[e_index] === target) {
+    //   return e_index;
+    // } 
+    else if (nums[pivot_index] < target) {
+      s_index = pivot_index + 1
+    } else {
+      e_index = pivot_index - 1;
+    }
+  }
+  return -1
 }
 
 module.exports = { binary_search, testCase };
--- test_runner.js
+++ test_runner.js
@@ -2,6 +2,7 @@ const test_runner = (custom_function, testCases) => {
     console.log(testCases
         .map(testData => ({
             ...testData,
+            input:JSON.stringify(testData.input),
             custom_function_output: custom_function(...testData.input),
         })))
 };
